a36e4a7618e872cdbc167f3a5f02a8e125478e7b
Fernando  Lopez Cardenas <ferlopezcardenas@gmail.com>
2023-09-19 00:57:18 -0600

    EOD

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
-import Button from "@/components/ui/Button";
-import useGlobalStore from "@/lib/store";
 import useAuthGuard from "@/hooks/useAuthGuard";
-import { initializeAuth, logout } from "@/lib/auth";
+import { initializeAuth } from "@/lib/auth";
 import Nav from "@/components/ui/Nav";
 
 initializeAuth();
 
 export default function Home() {
   const loading = useAuthGuard();
-  const user = useGlobalStore((store) => store.user);
-
   if (loading) return <p>Loading...</p>;
 
   return (
